ec443815c7711a037abaa3a6e37651c3a30ff221
dongig.shin <dongig.shin@samsung.com>
2025-07-02 14:05:13 +0900

    [refactor] 1. 2s 0b

--- baseball/baseball.cpp
+++ baseball/baseball.cpp
@@ -13,12 +13,16 @@ public:
 		if (guessNumber == question) {
 			return { true, 3, 0 };
 		}
-		if ((guessNumber[0] == question[0] && guessNumber[1] == question[1] && guessNumber[2] != question[2]) ||
-			(guessNumber[0] == question[0] && guessNumber[1] != question[1] && guessNumber[2] == question[2]) ||
-			(guessNumber[0] != question[0] && guessNumber[1] == question[1] && guessNumber[2] == question[2])) {
+		if (isStrike2Ball1(guessNumber)) {
 			return { false, 2, 0 };
 		}
 		return { false, 0, 0 };
+	}
+	bool isStrike2Ball1(const std::string& guessNumber)
+	{
+		return (guessNumber[0] == question[0] && guessNumber[1] == question[1] && guessNumber[2] != question[2]) ||
+			(guessNumber[0] == question[0] && guessNumber[1] != question[1] && guessNumber[2] == question[2]) ||
+			(guessNumber[0] != question[0] && guessNumber[1] == question[1] && guessNumber[2] == question[2]);
 	}
 	void assertIllegalArgument(const std::string& guessNumber)
 	{
